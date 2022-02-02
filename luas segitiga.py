dbf570b65db101642f7146c8f6c37b5d34226610
Tri Ahmad Fauzin <96157230+triahmadfauzin@users.noreply.github.com>
2022-02-02 09:37:57 +0700

    Add files via upload

---
+++ luas segitiga.py
@@ -0,0 +1,10 @@
+# Menghitung luas segitiga
+
+
+alas = input(' panjang alas segitiga: ')
+
+tinggi = input(' tinggi segitiga: ')
+
+hasil = float(1/2) * float(alas) * float(tinggi)
+
+print(' luas segitiga anda adalah {}'. format(hasil))
